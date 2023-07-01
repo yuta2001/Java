5732e6c3ec06045a8205def0abb7e1495c45d0f0
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-02 00:48:31 +0900

    p01

--- src/practice01.java
+++ src/practice01.java
@@ -1,5 +1,8 @@
 public class Practice01 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello yuta");
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅3横幅5の長方形の面積は、" + c);
     }
 }
