8d979d0ce55aad0f17a74803c66fec680cd94a37
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-02 01:05:18 +0900

    第一章完了2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         System.out.println("Hello yuta!!");
         System.out.println(1 + 1);
 
