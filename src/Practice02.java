492422332a1e403705a3645230a60f577c6e40c8
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-03 01:07:57 +0900

    計算の文

---
+++ src/Practice02.java
@@ -0,0 +1,14 @@
+public class Practice02 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello!! 2023-7-3");
+
+        int a = 20;
+        int b;
+        int c;
+        b = a + 5;
+        c = a * b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
