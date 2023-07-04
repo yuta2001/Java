c85698b1cdb1d804093dfa28b2305865327ad720
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-05 00:45:14 +0900

    複雑な運勢占い

--- src/practice03.java
+++ src/practice03.java
@@ -9,6 +9,8 @@ public class practice03 {
             System.out.println("DVDを見ます");
         }
 
+        // -------------------------------------------------------------------------------------//
+
         String name = "yuta";
 
         // while (name == "yuta") {
@@ -32,6 +34,50 @@ public class practice03 {
             System.out.println("テストです");
         } else {
             System.out.println("テスト2です");
+        }
+
+        // -------------------------------------------------------------------------------------//
+
+        int age = 10;
+        // 否定演算子
+        if (!(age == 10)) {
+            System.out.println("OKです!!");
+        } else {
+            System.out.println("NOです!!");
+        }
+
+        // -------------------------------------------------------------------------------------//
+
+        int Drink = 150;
+
+        if (Drink >= 170) {
+            System.out.println("Lサイズです");
+        } else if (Drink >= 150) {
+            System.out.println("Mサイズです");
+        } else {
+            System.out.println("Sサイズです");
+        }
+
+        // --------------------------------------------------------------------------------------//
+
+        System.out.println("あなたの運勢を占います");
+        System.out.println("あなたの名前を入力してください");
+        String you_name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("あなたの年齢を入力して下さい");
+        int you_age = new java.util.Scanner(System.in).nextInt();
+        System.out.println("結果が出ました!!OKと入力してください");
+        String result = new java.util.Scanner(System.in).nextLine();
+
+        int fortune = new java.util.Random().nextInt(4) + 1;
+
+        if (fortune == 1) {
+            System.out.println("大吉です");
+        } else if (fortune == 2) {
+            System.out.println("中吉です");
+        } else if (fortune == 3) {
+            System.out.println("小吉です");
+        } else {
+            System.out.println("凶です");
         }
     }
 }
