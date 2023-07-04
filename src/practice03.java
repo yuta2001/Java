62a590d29af26757134d7814f821eedca3a67a56
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-05 00:51:19 +0900

    switch分

--- src/practice03.java
+++ src/practice03.java
@@ -70,14 +70,30 @@ public class practice03 {
 
         int fortune = new java.util.Random().nextInt(4) + 1;
 
-        if (fortune == 1) {
-            System.out.println("大吉です");
-        } else if (fortune == 2) {
-            System.out.println("中吉です");
-        } else if (fortune == 3) {
-            System.out.println("小吉です");
-        } else {
-            System.out.println("凶です");
+        // if (fortune == 1) {
+        // System.out.println("大吉です");
+        // } else if (fortune == 2) {
+        // System.out.println("中吉です");
+        // } else if (fortune == 3) {
+        // System.out.println("小吉です");
+        // } else {
+        // System.out.println("凶です");
+        // }
+
+        // switch文ver
+        switch (fortune) {
+            // この場合case1,case2共に中吉になる
+            case 1:
+                // System.out.println("大吉です");
+                // break;
+            case 2:
+                System.out.println("中吉です");
+                break;
+            case 3:
+                System.out.println("小吉です");
+                break;
+            default:
+                System.out.println("凶です");
         }
     }
 }
