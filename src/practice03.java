13cd66ec4d3c660b800f1864cca5e15fae58e372
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-04 23:32:58 +0900

    条件分岐

---
+++ src/practice03.java
@@ -0,0 +1,18 @@
+public class practice03 {
+    public static void main(String[] args) throws Exception {
+        boolean whether = true;
+
+        if (whether == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩します");
+        } else {
+            System.out.println("DVDを見ます");
+        }
+
+        String name = "yuta";
+
+        while (name == "yuta") {
+            System.out.println("正解です!!");
+        }
+    }
+}
