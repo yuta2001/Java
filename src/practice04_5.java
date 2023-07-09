3e2b871fa5b1b02d578c08c8a30de78b8d01baa2
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-10 01:18:31 +0900

    多次元配列

--- src/practice04_5.java
+++ src/practice04_5.java
@@ -12,5 +12,12 @@ public class practice04_5 {
         // .length()・・・文字の長さを取得
         String str_test = "Javaの勉強中";
         System.out.println(str_test.length());
+
+        // 多次元配列
+
+        int[][] ary = { { 10, 20, 30, 40, 50 }, { 60, 70, 80, 90, 100 } };
+        System.out.println(ary.length);
+        // 一つ目の配列{10, 20, 30, 40, 50}の要素の長さが5つだから5と表示される
+        System.out.println(ary[0].length);
     }
 }
