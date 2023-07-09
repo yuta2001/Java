10ed6a606044265bab51c55dd7c48c09a6c71830
yuta2001 <ymaiiyuta0424@gmail.com>
2023-07-10 01:15:32 +0900

    null

---
+++ src/practice04_5.java
@@ -0,0 +1,16 @@
+public class practice04_5 {
+    public static void main(String[] args) {
+
+        // NullPointerException:null格納されている配列を使うと発生する。
+
+        // int[] ary = { 1, 2, 3, 4, 5, 6, 7 };
+        // ary = null;
+        // ary[0] = 10;
+
+        // ----------------------------------------------------------------
+
+        // .length()・・・文字の長さを取得
+        String str_test = "Javaの勉強中";
+        System.out.println(str_test.length());
+    }
+}
